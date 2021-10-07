b980a1dcb1deea8857476e364d973520180f33a8
waninger <mikael.waninger@gmail.com>
2021-10-07 15:37:50 +0200

    sessionservlet

--- src/main/java/com/example/lab1/SessionServlet.java
+++ src/main/java/com/example/lab1/SessionServlet.java
@@ -2,6 +2,7 @@ package com.example.lab1;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebServlet(name = "SessionServlet", value = "/SessionServlet")
 public class SessionServlet extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String name = request.getParameter("name");
